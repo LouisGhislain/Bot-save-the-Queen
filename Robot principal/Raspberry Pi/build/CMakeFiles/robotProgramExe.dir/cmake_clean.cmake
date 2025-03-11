53045bba590e8da556dc70de4105f611df4af494
Quantoum <q.depierpont@student.uclouvain.be>
2025-03-11 18:59:44 +0100

    nouveau cmake

---
+++ Robot principal/Raspberry Pi/build/CMakeFiles/robotProgramExe.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robotProgramExe.dir/src/Motor.cpp.o"
+  "CMakeFiles/robotProgramExe.dir/src/Motor.cpp.o.d"
+  "CMakeFiles/robotProgramExe.dir/src/Robot.cpp.o"
+  "CMakeFiles/robotProgramExe.dir/src/Robot.cpp.o.d"
+  "CMakeFiles/robotProgramExe.dir/src/Screen.cpp.o"
+  "CMakeFiles/robotProgramExe.dir/src/Screen.cpp.o.d"
+  "CMakeFiles/robotProgramExe.dir/src/main.cpp.o"
+  "CMakeFiles/robotProgramExe.dir/src/main.cpp.o.d"
+  "robotProgramExe"
+  "robotProgramExe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robotProgramExe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Robot principal/Raspberry Pi/build/CMakeFiles/robotProgramExe.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for robotProgramExe.
+# This may be replaced when dependencies are built.
