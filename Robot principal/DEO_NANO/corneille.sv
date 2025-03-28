c213b417d6ead27d1f0278b5b1f9a6e556aab196
LouisGhislain <louis.ghislain@student.uclouvain.be>
2025-03-28 15:01:25 +0100

    100µs speed resolution

--- Robot principal/DEO_NANO/corneille.sv
+++ Robot principal/DEO_NANO/corneille.sv
@@ -6,7 +6,7 @@ module EncoderSpeed (
     output logic signed [31:0] speed
 );
     // Constants
-    int TIMER_MAX = 500000;
+    int TIMER_MAX = 5000; // 100 µs @ 50 MHz
 
     // Internal variables for tick counting and time interval
     logic signed [31:0] tick_count;
