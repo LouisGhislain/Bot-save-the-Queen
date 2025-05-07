16cde56bc7d161926f0f6af29c7153cd26e0be6b
Quantoum <q.depierpont@student.uclouvain.be>
2025-05-07 19:36:30 +0200

    removed git ignore temporarily

---
+++ Robot principal/Raspberry Pi/CMakeFiles/exeRobotProgram.dir/src/main_test_2.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/exeRobotProgram.dir/src/main_test_2.cpp.o: \
+ /home/TheQueen/Github_European_Queen/Bot-save-the-Queen/Robot\ principal/Raspberry\ Pi/src/main\ test\ 2.cpp \
+ /usr/include/stdc-predef.h
