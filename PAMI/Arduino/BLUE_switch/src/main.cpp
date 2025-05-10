ea1fb2088ee539f8b811dc35f099ebd96b4f15cc
Romain Vanden Berghe <romain.vandenberghe@student.uclouvain.be>
2025-05-10 11:07:15 +0200

    code switch

--- PAMI/Arduino/BLUE_switch/src/main.cpp
+++ PAMI/Arduino/BLUE_switch/src/main.cpp
@@ -14,6 +14,7 @@
 #define DIR1 7
 #define DIR4 11
 #define DIR3 10
+#define BUZZ 13
 
 
 // const float TIME_INTERVAL = 0.0005; // 500µs en secondes
@@ -23,7 +24,7 @@
 // int iteration = 0;
 
 // Caractère Team en fonction de la couleur de l'équipe
-#define TEAM_COLOR 'Y' // 'Y' pour orange, 'B' pour bleu
+// #define TEAM_COLOR 'Y' // 'Y' pour orange, 'B' pour bleu
 // #define TEAM_COLOR 'B'
 
 
@@ -45,12 +46,50 @@ double startGame;
 int role;
 Robot SuperStar;
 
+char TEAM_COLOR; // 'Y' pour orange, 'B' pour bleu
+
 void setup() {
     Serial.begin(9600);
 
     while (analogRead(A5) < 1.5) {
         Serial.println("Waiting for the microswitch to be pressed...");
         delay(100); // Attendre un peu avant de vérifier à nouveau
+    }
+
+    int sensorValueSum = 0;
+    int numReadings = 10;  // nombre d'échantillons sur 1 seconde
+    int threshold = 800;    // seuil pour distinguer HIGH/LOW (sur 10 bits ADC)
+
+    for (int i = 0; i < numReadings; i++) {
+        sensorValueSum += analogRead(A1);
+        Serial.print("Valeur de la broche A1 : ");
+        Serial.println(analogRead(A1)); // Afficher la valeur lue sur le moniteur série
+        Serial.print(sensorValueSum);
+        delay(100);  // 100 échantillons x 10ms = 1000ms = 1s
+    }
+
+    int averageValue = sensorValueSum / numReadings;
+    Serial.print("Valeur moyenne de la broche A1 : ");
+    Serial.println(averageValue); // Afficher la valeur lue sur le moniteur série
+
+    if (averageValue < threshold) {  // team jaune
+        Serial.println("team jaune");
+        digitalWrite(BUZZ, HIGH);
+        delay(100);
+        digitalWrite(BUZZ, LOW);
+
+        TEAM_COLOR = 'Y';
+    } else {  // team bleu
+        Serial.println("team bleu");
+        digitalWrite(BUZZ, HIGH);
+        delay(100);
+        digitalWrite(BUZZ, LOW);
+        delay(500);
+        digitalWrite(BUZZ, HIGH);
+        delay(100);
+        digitalWrite(BUZZ, LOW);
+
+        TEAM_COLOR = 'B';
     }
 
     while (analogRead(A5) > 1.5) {
@@ -64,7 +103,7 @@ void setup() {
     Serial.println("Début du test de vitesse");
 
     startGame = millis();
-    delay(86000);  // Attendre que le moniteur série soit prêt
+    delay(6000);  // Attendre que le moniteur série soit prêt
 
     role = 0; // 0 pour le robot, 1 pour la superstar
     SuperStar = {STRAIGHT1, 0, false, false, false, false, false, false}; // Initialisation de l'état du robot
@@ -80,10 +119,15 @@ void loop(){
     // rightMotor.set_motor(9);
     // pami.lowlevelcontrol(0.3, 0.3); // Arrêter le robot
     if (pami.target_reached == false){
-        if (digitalRead(A5) == LOW) { // a changer 
+        if (TEAM_COLOR == 'Y') { // a changer 
+            Serial.println("team jaune");
             pami.middlecontrol(0.9, -0.43, 0.0, false, startGame);  // Avancer vers x = 1.0
-        } else {
+        } else if (TEAM_COLOR == 'B') { // a changer
+            Serial.println("team bleu");
             pami.middlecontrol(0.95, 0.45, 0.0, false, startGame);  // Avancer vers x = 1.0
+        }
+        else {
+            Serial.println("Erreur de couleur d'équipe !");
         }
     } else {
         pami.turnTail();  // Agiter la queue
@@ -91,7 +135,9 @@ void loop(){
         rightMotor.set_motor(0);
     }
 
-
+    // int data = analogRead(A1); // Lire la valeur de la broche A5
+    // Serial.print("Valeur de la broche A1 : ");
+    // Serial.println(data); // Afficher la valeur lue sur le moniteur série
 
     }
 
